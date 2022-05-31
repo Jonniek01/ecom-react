e36625797e0185d1d9d2733190732207aaeb0bc4
jonniek01 <jontejonhn@gmail.com>
2022-05-31 12:50:21 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navigation from './components/Navigation';
 import Product1 from './images/image-product-1.jpg'
 import Product2 from './images/image-product-2.jpg'
 import Product3 from './images/image-product-3.jpg'
-import Product4 from './images/image-product-3.jpg'
+import Product4 from './images/image-product-4.jpg'
 
 import Thumb1 from "./images/image-product-1-thumbnail.jpg"
 import Thumb2 from "./images/image-product-2-thumbnail.jpg"
@@ -15,31 +15,88 @@ import Thumb4 from "./images/image-product-4-thumbnail.jpg"
 
 import Next from './images/icon-next.svg'
 import Previous from './images/icon-previous.svg'
+import { useState } from 'react';
 
 
 
 function App() {
+  const [count, setCount]=useState(0)
+  const [cartCount, setCartCount]=useState(0)
+
+  const [Product, setProduct]=useState(Product1)
+  const [overlay, setOverlay]=useState('overlay-none')
+  const [image, setImage]=useState(Product)
+  const [imagecount, setImagecount]=useState(0)
+
+  let images=[Product1,Product2,Product3,Product4]
+  function nextImage(){
+    if(imagecount<3){
+      setImagecount(imagecount+1);
+      setImage(images[imagecount])
+    }
+    else
+      {
+        setImagecount(0);
+        setImage(images[imagecount])
+      }
+    
+  }
+  function prevImage(){
+    if(imagecount>0){
+      setImagecount(imagecount-1);
+      setImage(images[imagecount])
+    }
+    else
+      {
+        setImagecount(3);
+        setImage(images[imagecount])
+      }
+    
+  }
+
+
+
+ function plus(){
+    setCount(count+1)
+  }
+  function minus(){
+    if(count>0){
+      setCount(count-1)
+
+
+    }
+  }
+  function add(){
+    setCartCount(cartCount+count)
+    setCount(0)
+
+
+  }
+  function checkout(){
+    setCartCount(0)
+
+  }
   return (
     <div className="App">
-      <Navigation/>
+      <Navigation cartCount={cartCount} checkout={checkout}/>
     <div className="contentcontainer">
     <div className="content">
       <div className="left">
-        <img src={Product1} alt="" className="product"></img>
-        <div className="arrows"><img src={Previous} alt="" className="prev"></img><img src={Next} alt="" className="next"></img></div>
+        <img src={image} onClick={()=>{setOverlay('overlay-show')}} alt="" className="product"></img>
+        <div className="arrows"><img src={Previous} alt="" onClick={prevImage} style={{cursor:"pointer"}} className="prev"></img><img src={Next} alt="" onClick={nextImage} style={{cursor:"pointer"}}  className="next"></img></div>
         <ul className="thumbnails">
           <li>
-            <img src={Thumb1} alt=""></img>
+            <img onClick={()=>{setProduct(Product1);setImage(Product)}} src={Thumb1} alt=""></img>
           </li>
 
           <li>
-            <img src={Thumb2} alt=""></img>
+            <img onClick={()=>{setProduct(Product2);setImage(Product)}} src={Thumb2} alt=""></img>
           </li>
           <li>
-            <img src={Thumb3} alt=""></img>
+            <img onClick={()=>{setProduct(Product3);setImage(Product)}} src={Thumb3} alt=""></img>
           </li>
           <li>
-            <img src={Thumb4} alt=""></img>
+            <img onClick={()=>{setProduct(Product4);setImage(Product)}} src={Thumb4} alt=""></img>
           </li>
 
         </ul>
@@ -56,13 +113,41 @@ function App() {
           <p className="striked" >$250.00</p>
         </div>
         <div className="cartdiv">
-        <p className='pone'><span className="pl">-</span><span>0</span><span className="mn">+</span></p>
-        <p className="ptwo"><img src={CartIcon} style={{ color:"white",height:"15px"}} height="15px" alt=""></img><span>Add to cart</span></p>
+        <p className='pone'><span className="pl" onClick={minus} >-</span><span>{count}</span><span className="mn" onClick={plus}>+</span></p>
+        <p className="ptwo" onClick={add} ><img src={CartIcon} style={{ color:"white",height:"15px"}} height="15px" alt=""></img><span>Add to cart</span></p>
       </div>
 
       </div>
 
     </div>
+
+  </div>
+  <div className={overlay}>
+    <div className='over-content'>
+      <div className='close-over'><p style={{cursor:"pointer"}} onClick={()=>{setOverlay('overlay-none')}}>X</p></div>
+
+  <img className='overlay-image' src={image} alt=""></img>
+  <div className="change"><img src={Previous} alt="" onClick={prevImage} style={{cursor:"pointer"}} className="prev"></img><img src={Next} alt="" onClick={nextImage} style={{cursor:"pointer"}}  className="next"></img></div>
+  <ul className="thumbnails" id='over-thumbs'>
+          <li>
+            <img onClick={()=>{setProduct(Product1);setImage(Product)}} src={Thumb1} alt=""></img>
+          </li>
+
+          <li>
+            <img onClick={()=>{setProduct(Product2);setImage(Product)}} src={Thumb2} alt=""></img>
+          </li>
+          <li>
+            <img onClick={()=>{setProduct(Product3);setImage(Product)}} src={Thumb3} alt=""></img>
+          </li>
+          <li>
+            <img onClick={()=>{setProduct(Product4);setImage(Product)}} src={Thumb4} alt=""></img>
+          </li>
+
+        </ul>
+
+  </div>
+
+
 
   </div>
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import '../css/Navigation.css'
+import React, { useEffect } from 'react'
+import '../App.css'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import CartIcon from '../images/icon-cart.svg'
 import Avatar from '../images/image-avatar.png'
 import Close from '../images/icon-close.svg'
 import Menu from '../images/icon-menu.svg'
-function Navigation() {
+import Thumb1 from "../images/image-product-1-thumbnail.jpg"
+import Delete from '../images/icon-delete.svg'
+
+
+function Navigation({cartCount, checkout}) {
   const [sidebar, setSidebar]=useState('sidebarnone')
+  const[total, setTotal]=useState(0);
+  const [cartClass, setCartClass]=useState('cart-none')
+
+
+  useEffect(
+    ()=>{
+      setTotal(cartCount*125)
+   
+
+    },[cartCount]
+  )
   return (
     <div className="nav">
     <div className="navdesk">
@@ -32,7 +47,11 @@ function Navigation() {
 
       </ul>
       <div className="icons">
-        <img src={CartIcon} alt="" className="cart"></img>
+        <img onClick={()=>{
+          cartClass=='cart-none'?
+          setCartClass('cart-show'):
+          setCartClass('cart-none')
+          }} src={CartIcon} alt="" className="cart"></img>
         <img src={Avatar} alt="" className="avatar"></img>
       </div>
     </div>
@@ -40,7 +59,13 @@ function Navigation() {
       <img src={Menu} onClick={()=>{setSidebar('sidebar')}} id="menu" alt="" className="menu"></img>
       <h2 className="logo">sneakers</h2>
       <div className="icons">
-        <img src={CartIcon} alt="" className="cart"></img>
+        <img src={CartIcon} alt=""
+        onClick={()=>{
+          cartClass=='cart-none'?
+          setCartClass('cart-show'):
+          setCartClass('cart-none')
+          }}
+           className="cart"></img>
         <img src={Avatar} alt="" className="avatar"></img>
       </div>
       <div className={sidebar} id="sidebar">
@@ -68,9 +93,40 @@ function Navigation() {
 
       </div>
     </div>
+    <div className={cartClass}>
+      {
+        cartCount===0?
+        <div className='cart-card'>
+           <div className='cart-head'><p>Cart</p></div>
+          <div className='cart-body'>
+            <p>Your cart is empty</p>
+            </div>
+          </div>
+        :
+        <div className='cart-card'>
+          <div className='cart-head'><p>Cart</p></div>
+          <div className='cart-body'>
+              <img className='cart-thumb' src={Thumb1} alt="" />
+              <div className='fall'>
+                <p>Fall Limited Edition Sneakers</p>
+                <p><span>$125</span>&#215;<span>{cartCount} </span><span>{total}</span></p>
+            
+            </div>
+            <img src={Delete} onClick={()=>{ checkout()}} style={{height:"20px",cursor:"pointer"}} alt="" />
+            </div>
+            <div  className='checkout'><button onClick={()=>{ checkout()}}>Checkout</button></div>
+
+
+
+        </div>
+      }
+
+
+    </div>
   </div>
 
   )
 }
+export default Navigation;
 
-export default Navigation
+
